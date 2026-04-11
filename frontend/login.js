d124bca0c48fef519e7ea9c08446667fca92e3ae
sujal bekariya <sujalbekariya06@gmail.com>
2026-04-11 13:18:06 +0530

    Hello

--- frontend/login.js
+++ frontend/login.js
@@ -71,7 +71,8 @@ async function sendOTP() {
             document.getElementById('step1').classList.add('hidden');
             document.getElementById('step2').classList.remove('hidden');
             document.getElementById('otpSentMsg').innerText =
-                `OTP bheja +91 ${phone} pe. (Testing OTP: ${data.otp})`;
+                `OTP sent on +91 ${phone}
+                // (Testing OTP: ${data.otp})`;
             startResendTimer(); // 30 second countdown shuru karo
         } else {
             alert(data.error || 'Cant send OTP');
